d138559fc5947ef19c20c853c7801d3f60c448e4
Anwesa <anwesaray26@gmail.com>
2025-04-29 23:20:29 +0530

    commitv1 29-04

--- src/components/PlatfromLogoCarousal.jsx
+++ src/components/PlatfromLogoCarousal.jsx
@@ -1,54 +1,72 @@
 import React from 'react';
-import { Carousel, Row, Col } from 'react-bootstrap';
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 
-const logos = [
-  { image: "images/fansly_light_v3-1024x286.webp", name: "Fansly" },
-  { image: "images/1024fans.png", name: "1024FANS" },
-  { image: "images/flirtback-logo-light.png", name: "FlirtBack" },
-  { image: "images/loyalfans.png", name: "LoyalFans" },
-  { image: "images/peach.49f1951.png", name: "Peach" },
-  { image: "images/unfilter-1024x182.png", name: "Unflitrd" },
-  { image: "images/xgirl-hub.png", name: "XGirlHub" },
-  { image: "images/logo123.png", name: "XXX Follow" }
-];
 
-// Helper to group logos into sets of 4 per slide
-const chunkArray = (array, size) => {
-  const result = [];
-  for (let i = 0; i < array.length; i += size) {
-    result.push(array.slice(i, i + size));
-  }
-  return result;
-};
 
-const PlatformLogoCarousel = () => {
-  const groupedLogos = chunkArray(logos, 4); 
 
+const PlatformLogoCarouse = () => {
+  const settings = {
+    dots: false,
+    loop: true,
+    arrows: true,
+    infinite: true,
+    speed: 600,
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 5000,
+    responsive: [
+      {
+        breakpoint: 991,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+        }
+      },
+      {
+        breakpoint: 767,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        }
+      },
+      {
+        breakpoint: 575,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        }
+      },
+    ]
+  };
+  const logos = [
+    { image: "images/fansly_light_v3-1024x286.webp", name: "Fansly" },
+    { image: "images/1024fans.png", name: "1024FANS" },
+    { image: "images/flirtback-logo-light.png", name: "FlirtBack" },
+    { image: "images/loyalfans.png", name: "LoyalFans" },
+    { image: "images/peach.49f1951.png", name: "Peach" },
+    { image: "images/unfilter-1024x182.png", name: "Unflitrd" },
+    { image: "images/xgirl-hub.png", name: "XGirlHub" },
+    { image: "images/logo123.png", name: "XXX Follow" }
+  ];
   return (
-    <Carousel className="platform-carousel">
-      {groupedLogos.map((group, idx) => (
-        <Carousel.Item key={idx}>
-          <Row className="justify-content-center py-4">
-            {group.map((logo, index) => (
-              <Col key={index} xs={6} md={3} className="text-center">
-                <PlatformLogo image={logo.image} name={logo.name} />
-              </Col>
-            ))}
-          </Row>
-        </Carousel.Item>
-      ))}
-    </Carousel>
+    <Slider {...settings} className="">
+    {/* all_categories owl-carousel owl-theme */}
+    {logos.map((logos, index) => (
+      <div className="item" key={index}>
+      <figure>
+        <img src={logos.image} alt={logos.name} />
+      </figure>
+      <p>{logos.name}</p>
+      </div>
+    ))}
+  </Slider>
   );
 };
 
-const PlatformLogo = ({ image, name }) => (
-  <li className="item">
-    <figure>
-      <img src={image} alt={name} />
-    </figure>
-    <p>{name}</p>
-  </li>
-);
 
-export default PlatformLogoCarousel;
+
+export default PlatformLogoCarouse;
