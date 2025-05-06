6f59914714f69d6d1cb6bcdb898bc5ba0ca588c9
Anwesa <anwesaray26@gmail.com>
2025-05-06 23:47:45 +0530

    seo pages content

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,7 @@
 import React from "react";
-import payImg from "../assets/images/payments-ezgif.png"
+import payImg from "../assets/images/payments-ezgif.png";
 
 const Footer = () => {
-
   const seoLinks = [
     { href: "/seo/philippines", label: "Adult SEO Services Philippines" },
     { href: "/seo/montenegro", label: "Adult SEO Services Montenegro" },
@@ -23,7 +22,7 @@ const Footer = () => {
     { href: "/seo/italy", label: "Adult SEO Services Italy" },
     { href: "/seo/singapore", label: "Adult SEO Services Singapore" },
     { href: "/seo/poland", label: "Adult SEO Services Poland" },
-    { href: "/seo/new-zealand", label: "Adult SEO Services New Zealand" },
+    { href: "/seo/newzealand", label: "Adult SEO Services New Zealand" },
     { href: "/seo/japan", label: "Adult SEO Services Japan" },
     { href: "/seo/hungary", label: "Adult SEO Services Hungary" },
     { href: "/seo/finland", label: "Adult SEO Services Finland" },
@@ -40,15 +39,14 @@ const Footer = () => {
     { href: "/seo/sweden", label: "Adult SEO Services Sweden" },
     { href: "/seo/ireland", label: "Adult SEO Services Ireland" },
     { href: "/seo/china", label: "Adult SEO Services China" },
-    { href: "/seo/adult-seo-luxembourg", label: "Adult SEO Services Luxembourg" },
-    { href: "/seo/escort-marketing-netherlands", label: "Adult SEO Services Netherlands" },
+    { href: "/seo/luxembourg", label: "Adult SEO Services Luxembourg" },
+    { href: "/seo/netherlands", label: "Adult SEO Services Netherlands" },
     { href: "/seo/australia", label: "Adult SEO Services Australia" },
     { href: "/seo/tanzania", label: "Adult SEO Services Tanzania" },
     { href: "/seo/uruguay", label: "Adult SEO Services Uruguay" },
     { href: "/seo/armenia", label: "Adult SEO Services Armenia" },
     { href: "/seo/usa", label: "Adult SEO Services USA" },
   ];
-
 
   return (
     <footer className="footer">
@@ -80,8 +78,7 @@ const Footer = () => {
                   <div className="rt_dtls">
                     <p>
                       ITHUM TOWER, A-153, Tower B,
-                      <br />
-                      5<sup>th</sup> Floor, Noida, UP, 201309
+                      <br />5<sup>th</sup> Floor, Noida, UP, 201309
                     </p>
                   </div>
                 </li>
@@ -114,7 +111,11 @@ const Footer = () => {
             <div className="row pt-3 pb-3">
               <div className="col-lg-4">
                 <div className="outer_logo">
-                  <img src="/images/01.jpeg" className="img-fluid" alt="Logo 1" />
+                  <img
+                    src="/images/01.jpeg"
+                    className="img-fluid"
+                    alt="Logo 1"
+                  />
                 </div>
               </div>
               <div className="col-lg-3">
@@ -133,20 +134,54 @@ const Footer = () => {
           <div className="col-lg-3 pt-3">
             <h3>Quick Links</h3>
             <ul className="quick_link">
-              <li><a href="/about-us">About</a></li>
-              <li><a href="/faqs">FAQ</a></li>
-              <li><a href="/blog">Blog</a></li>
-              <li><a href="/pricing">Pricing</a></li>
-              <li><a href="/contact-us">Contact</a></li>
-              <li><a href="/careers">Careers</a></li>
-              <li><a href="/portfolio">Portfolio</a></li>
-              <li><a href="/ema-guest-blogging-guidelines">Guest Blogging Guidelines</a></li>
-              <li><a href="/referral">Referral</a></li>
-              <li><a href="/important-update-regarding-scammers">Important Update Regarding Scammers</a></li>
-              <li><a href="/sitemap">Sitemap</a></li>
-              <li><a href="/privacy-policy">Privacy Policy</a></li>
-              <li><a href="/request-free-seo-analysis">Request Free SEO Analysis</a></li>
-              <li><a href="/social-feed">Social Feed</a></li>
+              <li>
+                <a href="/about-us">About</a>
+              </li>
+              <li>
+                <a href="/faqs">FAQ</a>
+              </li>
+              <li>
+                <a href="/blog">Blog</a>
+              </li>
+              <li>
+                <a href="/pricing">Pricing</a>
+              </li>
+              <li>
+                <a href="/contact-us">Contact</a>
+              </li>
+              <li>
+                <a href="/careers">Careers</a>
+              </li>
+              <li>
+                <a href="/portfolio">Portfolio</a>
+              </li>
+              <li>
+                <a href="/ema-guest-blogging-guidelines">
+                  Guest Blogging Guidelines
+                </a>
+              </li>
+              <li>
+                <a href="/referral">Referral</a>
+              </li>
+              <li>
+                <a href="/important-update-regarding-scammers">
+                  Important Update Regarding Scammers
+                </a>
+              </li>
+              <li>
+                <a href="/sitemap">Sitemap</a>
+              </li>
+              <li>
+                <a href="/privacy-policy">Privacy Policy</a>
+              </li>
+              <li>
+                <a href="/request-free-seo-analysis">
+                  Request Free SEO Analysis
+                </a>
+              </li>
+              <li>
+                <a href="/social-feed">Social Feed</a>
+              </li>
             </ul>
           </div>
 
@@ -154,23 +189,63 @@ const Footer = () => {
           <div className="col-lg-3 pt-3">
             <h3>Services</h3>
             <ul className="quick_link">
-              <li><a href="/adult-seo/">Adult SEO</a></li>
-              <li><a href="/adult-e-commerce/">Adult E-Commerce SEO</a></li>
-              <li><a href="/escort-seo/">Escort SEO</a></li>
-              <li><a href="/escort-social-media-marketing/">Escort Social Media Marketing</a></li>
-              <li><a href="/adult-android-app-development-agency/">Adult Android App Development</a></li>
-              <li><a href="/adult-content-writing-services/">Adult Content Writing</a></li>
-              <li><a href="/strippers-seo/">Strippers SEO</a></li>
-              <li><a href="/adult-dating-seo/">Adult Dating SEO</a></li>
-              <li><a href="/webcams-seo/">Adult Webcams SEO</a></li>
-              <li><a href="/pornstar-seo/">Porn Star SEO</a></li>
-              <li><a href="/porn-tube-seo/">Tube SEO</a></li>
-              <li><a href="/erotic-massage-seo/">Erotic Massage SEO</a></li>
-              <li><a href="/brothel-seo/">Brothel SEO</a></li>
-              <li><a href="/adult-link-building/">Adult Link Building</a></li>
-              <li><a href="/adult-website-design/">Adult Web Design</a></li>
-              <li><a href="/independent-escort-seo/">Independent Escort SEO</a></li>
-              <li><a href="/escort-directory-seo/">Escort Directory SEO</a></li>
+              <li>
+                <a href="/adult-seo/">Adult SEO</a>
+              </li>
+              <li>
+                <a href="/adult-e-commerce/">Adult E-Commerce SEO</a>
+              </li>
+              <li>
+                <a href="/escort-seo/">Escort SEO</a>
+              </li>
+              <li>
+                <a href="/escort-social-media-marketing/">
+                  Escort Social Media Marketing
+                </a>
+              </li>
+              <li>
+                <a href="/adult-android-app-development-agency/">
+                  Adult Android App Development
+                </a>
+              </li>
+              <li>
+                <a href="/adult-content-writing-services/">
+                  Adult Content Writing
+                </a>
+              </li>
+              <li>
+                <a href="/strippers-seo/">Strippers SEO</a>
+              </li>
+              <li>
+                <a href="/adult-dating-seo/">Adult Dating SEO</a>
+              </li>
+              <li>
+                <a href="/webcams-seo/">Adult Webcams SEO</a>
+              </li>
+              <li>
+                <a href="/pornstar-seo/">Porn Star SEO</a>
+              </li>
+              <li>
+                <a href="/porn-tube-seo/">Tube SEO</a>
+              </li>
+              <li>
+                <a href="/erotic-massage-seo/">Erotic Massage SEO</a>
+              </li>
+              <li>
+                <a href="/brothel-seo/">Brothel SEO</a>
+              </li>
+              <li>
+                <a href="/adult-link-building/">Adult Link Building</a>
+              </li>
+              <li>
+                <a href="/adult-website-design/">Adult Web Design</a>
+              </li>
+              <li>
+                <a href="/independent-escort-seo/">Independent Escort SEO</a>
+              </li>
+              <li>
+                <a href="/escort-directory-seo/">Escort Directory SEO</a>
+              </li>
             </ul>
           </div>
 
@@ -178,27 +253,57 @@ const Footer = () => {
           <div className="col-lg-2 pt-3">
             <h3>Follow Us</h3>
             <ul className="quick_link">
-              <li><a href="https://twitter.com/EscortSEOAgency" target="_blank" rel="noopener noreferrer">X (Twitter)</a></li>
-              <li><a href="https://www.instagram.com/escortmarketingagency/" target="_blank" rel="noopener noreferrer">Instagram</a></li>
-              <li><a href="https://www.linkedin.com/company/escort-marketing-agency/" target="_blank" rel="noopener noreferrer">LinkedIn</a></li>
-              <li><a href="skype:live:.cid.3022ec909df75187?chat">Skype</a></li>
-              <li><a href="/feed/">RSS</a></li>
+              <li>
+                <a
+                  href="https://twitter.com/EscortSEOAgency"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  X (Twitter)
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://www.instagram.com/escortmarketingagency/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Instagram
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://www.linkedin.com/company/escort-marketing-agency/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  LinkedIn
+                </a>
+              </li>
+              <li>
+                <a href="skype:live:.cid.3022ec909df75187?chat">Skype</a>
+              </li>
+              <li>
+                <a href="/feed/">RSS</a>
+              </li>
             </ul>
           </div>
 
           {/* SEO Services Global */}
           <div className="col-lg-12 pt-3 mb-5">
-      <h3 style={{ textAlign: "center" }}>SEO Services for Global Community</h3>
-      <div className="row" style={{ color: "white" }}>
-        {seoLinks.map((link, index) => (
-          <div className="col-lg-4" key={index}>
-            <div className="quick_link">
-              <a href={link.href}>{link.label}</a>
+            <h3 style={{ textAlign: "center" }}>
+              SEO Services for Global Community
+            </h3>
+            <div className="row" style={{ color: "white" }}>
+              {seoLinks.map((link, index) => (
+                <div className="col-lg-4" key={index}>
+                  <div className="quick_link">
+                    <a href={link.href}>{link.label}</a>
+                  </div>
+                </div>
+              ))}
             </div>
           </div>
-        ))}
-      </div>
-    </div>
 
           {/* pay via */}
           <div className="row">
@@ -208,7 +313,9 @@ const Footer = () => {
                   <div className="col-lg-6">
                     <div id="footer-2">
                       <aside id="block-24" className="widget widget_block">
-                        <p>© 2023 Escort Marketing Agency. All Rights Reserved</p>
+                        <p>
+                          © 2023 Escort Marketing Agency. All Rights Reserved
+                        </p>
                       </aside>
                     </div>
                   </div>
@@ -222,7 +329,6 @@ const Footer = () => {
               </div>
             </div>
           </div>
-
         </div>
       </div>
     </footer>
