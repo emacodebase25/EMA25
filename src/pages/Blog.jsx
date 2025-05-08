a33d9e75f3ee7f7bb639d92bd0d8c4a803010a59
Anwesa <anwesaray26@gmail.com>
2025-05-08 23:47:58 +0530

    blog-page

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -20,15 +20,14 @@ const EscortBlogPage = () => {
       fetchBlogs(currentPage, 8);
     }
   }, [currentPage]);
-  
+
   useEffect(() => {
-    console.log(blogPosts)
+    console.log(blogPosts);
     if (currentPage === 1 && blogPosts.length > 0) {
       setTopFiveBlogs(blogPosts.slice(0, 5));
     }
     setTotalPages(9);
   }, [blogPosts, currentPage]);
-  
 
   const handlePageChange = (pageNum) => {
     if (pageNum >= 1 && pageNum <= totalPages) {
@@ -70,13 +69,13 @@ const EscortBlogPage = () => {
       </BannerSection>
 
       {/* Show blog card */}
-      <section className="inner_page_area">
+      <section className="inner_page_area blog_list_area">
         <div className="container">
           <div className="row mb-3">
             <div className="col-lg-6">
               <h3>Blog Articles</h3>
             </div>
-            <div className="col-lg-6">
+            <div className="col-lg-6 blg-search">
               <input
                 type="search"
                 className="form-control"
@@ -84,6 +83,22 @@ const EscortBlogPage = () => {
                 value={searchTerm}
                 onChange={(e) => setSearchTerm(e.target.value)}
               />
+              <span>
+                <svg
+                  width="22"
+                  height="22"
+                  viewBox="0 0 512 512"
+                  enable-background="new 0 0 512 512"
+                >
+                  <path
+                    d="M460.355,421.59L353.844,315.078c20.041-27.553,31.885-61.437,31.885-98.037
+						C385.729,124.934,310.793,50,218.686,50C126.58,50,51.645,124.934,51.645,217.041c0,92.106,74.936,167.041,167.041,167.041
+						c34.912,0,67.352-10.773,94.184-29.158L419.945,462L460.355,421.59z M100.631,217.041c0-65.096,52.959-118.056,118.055-118.056
+						c65.098,0,118.057,52.959,118.057,118.056c0,65.096-52.959,118.056-118.057,118.056C153.59,335.097,100.631,282.137,100.631,217.041
+						z"
+                  ></path>
+                </svg>
+              </span>
             </div>
           </div>
 
